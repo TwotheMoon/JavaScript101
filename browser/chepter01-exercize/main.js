d5b483f0decc8f2079ee01881204850da5181cb9
2theMoon <dlaguddh1@gmail.com>
2021-12-21 16:44:32 +0900

    성능 업 버전
    - 최소한의 layout을 발생을 위해 수정

--- browser/chepter01-exercize/main.js
+++ browser/chepter01-exercize/main.js
@@ -3,14 +3,20 @@ const vertical = document.querySelector(".vertical");
 const target = document.querySelector(".target");
 const tag = document.querySelector(".tag");
 
-document.addEventListener("mousemove", (event) => {
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-    target.style.left = `${mouseX}px`;
-    target.style.top = `${mouseY}px`;
-    horizontal.style.top = `${mouseY}px`;
-    vertical.style.left = `${mouseX}px`;
-    tag.style.left = `${mouseX}px`;
-    tag.style.top = `${mouseY}px`;
-    tag.innerText = `${mouseX}px, ${mouseY}px`;
+addEventListener("load", () => { // window.onLoad
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
+
+
+    document.addEventListener("mousemove", (event) => {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+
+        vertical.style.transform = `translateX(${mouseX}px)`;
+        horizontal.style.transform = `translateY(${mouseY}px)`;
+        target.style.transform = `translate(${mouseX - targetHalfWidth}px, ${mouseY - targetHalfHeight}px)`;
+        tag.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
+        tag.innerText = `${mouseX}px, ${mouseY}px`;
+    });
 });
