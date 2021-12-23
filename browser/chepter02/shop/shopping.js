2cdeec42041288ded6dcdcdd3af16ab5ffc28bbe
2theMoon <dlaguddh1@gmail.com>
2021-12-23 15:08:00 +0900

    todo 연습

---
+++ browser/chepter02/shop/shopping.js
@@ -0,0 +1,39 @@
+const listForm = document.querySelector(".listForm");
+const listInput = document.querySelector(".listInput");
+const lists = document.querySelector(".lists");
+
+function handleForm(e) {
+    e.preventDefault();
+    const newList = listInput.value;
+
+    const newListObj = {
+        text: newList,
+        id: Date.now()
+    };
+
+    paintList(newListObj);
+    listInput.value = "";
+}
+
+function paintList(newListObj) {
+    const li = document.createElement("li");
+    li.id = newListObj.id;
+    const span = document.createElement("span");
+    span.innerText = `${newListObj.text}`;
+
+    const btn = document.createElement("button");
+    btn.innerText = "❌";
+
+    btn.addEventListener("click", deleteList);
+    li.appendChild(span);
+    li.appendChild(btn);
+    lists.appendChild(li);
+
+}
+
+function deleteList(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+listForm.addEventListener("submit", handleForm);
