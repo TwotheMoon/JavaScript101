af086926e8a2247b5b89e62b28a6874c4c1e77fd
2theMoon <dlaguddh1@gmail.com>
2022-01-11 14:11:09 +0900

    Array.of Array.from

--- nicoES6/index.js
+++ nicoES6/index.js
@@ -1,40 +1,57 @@
-const styled = (aElement) => {
-    const el = document.createElement(aElement);
-    return args => {
-        const styles = args[0];
-        el.style = styles;
-        return el;
-    }
-};
+// const styled = (aElement) => {
+//     const el = document.createElement(aElement);
+//     return args => {
+//         const styles = args[0];
+//         el.style = styles;
+//         return el;
+//     }
+// };
 
-const title = styled("h1")`
-    border-radius: 10px;
-    color: blue;
-`;
+// const title = styled("h1")`
+//     border-radius: 10px;
+//     color: blue;
+// `;
 
-const subtitle = styled("span")`
-    color: red;
-`;
+// const subtitle = styled("span")`
+//     color: red;
+// `;
 
-title.innerText = "We just cloned";
-subtitle.innerText = "styled";
+// title.innerText = "We just cloned";
+// subtitle.innerText = "styled";
 
-document.body.append(title, subtitle);
-
-
-includes
-const isEmail = (email) => email.includes("@");
-console.log(isEmail("dlaguddh@naver.com"));
+// document.body.append(title, subtitle);
 
 
-reoeat
-const CC_NUMBER = "6060";
-
-const displayName = `${"*".repeat(10)}${CC_NUMBER}`;
-
-console.log(displayName);
+// includes
+// const isEmail = (email) => email.includes("@");
+// console.log(isEmail("dlaguddh@naver.com"));
 
 
-startsWith()
-const name = "Mr. Nicolas";
-console.log(name.startsWith("Nicolas"));
+// reoeat
+// const CC_NUMBER = "6060";
+
+// const displayName = `${"*".repeat(10)}${CC_NUMBER}`;
+
+// console.log(displayName);
+
+
+// startsWith()
+// const name = "Mr. Nicolas";
+// console.log(name.startsWith("Nicolas"));
+
+
+// array Method
+// Array.of()
+const friends = Array.of("nico", "lynn", "dal", "mark");
+
+console.log(friends);
+
+
+// Array.from()
+
+const buttons = document.querySelectorAll("button");
+
+Array.from(buttons).forEach(button => {
+    button.addEventListener("click", () => console.log("yap!"));
+})
+console.log(buttons);
