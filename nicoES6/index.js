359f6277a6f925c0fec0e10dd6e8897094faa706
2theMoon <dlaguddh1@gmail.com>
2022-04-28 20:18:51 +0900

    test1

--- nicoES6/index.js
+++ nicoES6/index.js
@@ -1,40 +1,41 @@
-// const styled = (aElement) => {
-//     const el = document.createElement(aElement);
-//     return args => {
-//         const styles = args[0];
-//         el.style = styles;
-//         return el;
-//     }
-// };
+const styled = (aElement) => {
+    const el = document.createElement(aElement);
+    return args => {
+        const styles = args[0];
+        el.style = styles;
+        return el;
+    }
+};
 
-// const title = styled("h1")`
-//     border-radius: 10px;
-//     color: blue;
-// `;
+const title = styled("h1")`
+    border-radius: 10px;
+    color: blue;
+`;
 
-// const subtitle = styled("span")`
-//     color: red;
-// `;
+const subtitle = styled("span")`
+    color: red;
+`;
 
-// title.innerText = "We just cloned";
-// subtitle.innerText = "styled";
+title.innerText = "We just cloned";
+subtitle.innerText = "styled";
 
-// document.body.append(title, subtitle);
-
-
-// includes
-// const isEmail = (email) => email.includes("@");
-// console.log(isEmail("dlaguddh@naver.com"));
+document.body.append(title, subtitle);
 
 
-// reoeat
-// const CC_NUMBER = "6060";
-
-// const displayName = `${"*".repeat(10)}${CC_NUMBER}`;
-
-// console.log(displayName);
+includes
+const isEmail = (email) => email.includes("@");
+console.log(isEmail("dlaguddh@naver.com"));
 
 
+reoeat
+const CC_NUMBER = "6060";
+
+const displayName = `${"*".repeat(10)}${CC_NUMBER}`;
+
+console.log(displayName);
+
+
+<<<<<<< HEAD
 // startsWith()
 // const name = "Mr. Nicolas";
 // console.log(name.startsWith("Nicolas"));
@@ -54,3 +55,8 @@
 //     button.addEventListener("click", () => console.log("yap!"));
 // })
 // console.log(buttons);
+=======
+startsWith()
+const name = "Mr. Nicolas";
+console.log(name.startsWith("Nicolas"));
+>>>>>>> parent of af08692 (Array.of Array.from)
